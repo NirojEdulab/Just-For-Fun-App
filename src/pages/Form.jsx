3c98f4a1d3fe8d3d17e5e0b54d4c8af083c6f752
Niroj Kumar <kumarniroj86@gmail.com>
2023-03-02 16:08:41 +0530

    ui update #1

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 
 const Form = () => {
-  
+
   let API1 = "https://api.agify.io/?name="
   let API2 = "https://api.nationalize.io/?name="
   let API3 = "https://api.genderize.io/?name="
 
   const [name, setName] = useState('')
   const [disable, setDisable] = useState(1)
-  const [clicked, setClicked] = useState(0)
+  const [clicked, setClicked] = useState(1)
 
   const handleName = (e) => {
     const getName = e.target.value;
@@ -22,44 +22,44 @@ const Form = () => {
     }
 
   }
-  
+
   const resetButton = (e) => {
     setClicked(1)
   }
   const handleSubmit = (e) => {
-    if(name.length>=3){
+    if (name.length >= 3) {
       e.preventDefault();
       setDisable(1)
       setClicked(0)
-    }else{
+    } else {
       alert("Oops!!! Error Occured...")
     }
-    
+
   }
   const [data1, setData1] = useState([])
   const [data2, setData2] = useState([])
   const [data3, setData3] = useState([])
 
-  const apiGet1 = ()=>{
-    fetch(API1+name)
-    .then((response) => response.json())
-    .then((json)=>{
-      setData1(json)
-    })
-    fetch(API2+name)
-    .then((response) => response.json())
-    .then((json)=>{
-      setData2(json.country.slice(0,2))
-    })
-    fetch(API3+name)
-    .then((response) => response.json())
-    .then((json)=>{
-      setData3(json)
-    })
+  const apiGet1 = () => {
+    fetch(API1 + name)
+      .then((response) => response.json())
+      .then((json) => {
+        setData1(json)
+      })
+    fetch(API2 + name)
+      .then((response) => response.json())
+      .then((json) => {
+        setData2(json.country.slice(0, 2))
+      })
+    fetch(API3 + name)
+      .then((response) => response.json())
+      .then((json) => {
+        setData3(json)
+      })
   }
 
   return (
-    <main>
+    <div className="gamePage">
       <div className="pg-header">
         <div className="container">
           <h1>You can start now</h1>
@@ -79,31 +79,33 @@ const Form = () => {
           </form>
         </center>
       </div>
+      <div className="tableContainer">
       <table hidden={clicked} className="table">
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Gender</th>
-      <th>Age</th>
-      <th>Top Two Countries</th>
-    </tr>
-  </thead>
-  <tbody>
-    
-    <tr>
-      <th>{data1.name}</th>
-      <th>{JSON.stringify(data3.gender)}</th>
-      <th>{JSON.stringify(data1.age)}</th>
-      {data2.map((c, i)=>(
-        <td key ={i}>
-          {c.country_id+", Probability => "}
-          {c.probability}
-        </td>
-      ))}
-    </tr>
-  </tbody>
-</table>
-    </main>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+            <th>Top Two Countries</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          <tr>
+            <th>{data1.name}</th>
+            <th>{JSON.stringify(data3.gender)}</th>
+            <th>{JSON.stringify(data1.age)}</th>
+            {data2.map((c, i) => (
+              <td key={i}>
+                {c.country_id + ", Probability => "}
+                {c.probability}
+              </td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+      </div>
+    </div>
   )
 };
 
